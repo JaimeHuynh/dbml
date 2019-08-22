7cbb2e4c8aa1ed10c8e73facf12aa5bac25e7df7
khoa0319 <khoa0319@gmail.com>
2019-08-22 10:01:57 +0700

    remove jest project option in package.json

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "presets": ["@babel/preset-env"],
-  "env": {
-    "test": {
-      "plugins": [
-        "@babel/plugin-transform-runtime",
-      ]
-    }
-  }
-}
+  presets: ['@babel/preset-env'],
+  env: {
+    test: {
+      plugins: [
+        '@babel/plugin-transform-runtime',
+      ],
+    },
+  },
+};
